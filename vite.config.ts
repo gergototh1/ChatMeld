7215b97a5c29a655399b095347ddd01af6a33bc5
gergototh1 <76750702+gergototh1@users.noreply.github.com>
2025-08-06 17:12:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(({ command }) => ({
-  base: '/ChatMeld/',
+  base: './',
   build: {
     outDir: 'docs',
     assetsInlineLimit: Infinity,
